e614e660e4407db0c1abdccb0d2dbf011fd6fb71
miffi <michaelciociola123@gmail.com>
2018-10-01 22:41:32 -0400

    Updated _awesome to reflect new hotkeys

--- _awesomewm.py
+++ _awesomewm.py
@@ -6,31 +6,25 @@ class AwesomeRule(MappingRule):
     mapping = {
         'awesome run': Key('w-r/10'),
         'awesome restart': Key('ws-r'),
-        'open terminal': Key('w-enter'),
         'awesome menu': Key('w-w'),
         'awesome maximize': Key('w-m'),
         'awesome kill': Key('ws-c'),
-        'next window [<n>]': Key('w-j:%(n)d/5'),
-        'previous window [<n>]': Key('w-k:%(n)d/5'),
-        'awesome urgent': Key('w-u'),
+        'focus left [<n>]': Key('w-h:%(n)d/10'),
+        'focus right [<n>]': Key('w-l:%(n)d/10'),
+        'focus up [<n>]': Key('w-l:%(n)d/10'),
+        'focus down [<n>]': Key('w-j:%(n)d/10'),
 
-        'next (screen|monitor|display) [<n>]': Key('cw-j:%(n)d/5'),
-        'previous (screen|monitor|display) [<n>]': Key('cw-k:%(n)d/5'),
-        'awesome change (screen|monitor|display) [<n>]': Key('w-o:%(n)d/5'),
-
-        'awesome (switch|move)': Key('ws-j/5'),
-        'awesome (switch|move) last': Key('ws-k/5'),
-        'awesome [width] left [<n>]': Key('w-h:%(n)d/5'),
-        'awesome [width] right [<n>]': Key('w-l:%(n)d/5'),
+        'width increase [<n>]': Key('as-h:%(n)d/5'),
+        'width shrink [<n>]': Key('as-l:%(n)d/5'),
 
         'awesome layout': Key('w-space/5'),
-        'awesome swap': Key('w-space/5'),
 
         'desktop <n>': Key('w-%(n)d/10'),
-        'awesome tag <n>': Key('ws-%(n)d/5'),
+        'set tag <n>': Key('ws-%(n)d/5'),
 
         'open firefox': Key('w-r/10') + Text("firefox") + Key('enter/10'),
         'open workrave': Key('w-r/10') + Text("workrave") + Key('enter/10'),
+        'open terminal': Key('w-enter'),
 
 
     }
